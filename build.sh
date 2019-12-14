00af54cfb7baa9c727d003b048e8bf60ac19de33
probonopd <probonopd@users.noreply.github.com>
2019-12-14 11:33:01 +0000

    Enable Universe

--- build.sh
+++ build.sh
@@ -13,7 +13,11 @@
 # Build dependencies
 #############################################################################
 
-sudo apt-get -y install libcurl-dev python3-pyqt5 python3-zmq python3-numpy
+# Enable the Universe repository
+sudo add-apt-repository universe -y
+sudo apt-get update
+
+sudo apt-get -y install libcurl4-gnutls-dev python3-pyqt5 python3-zmq python3-numpy
 sudo pip3 install crccheck
 
 APPDIR=$(readlink -f appdir)
